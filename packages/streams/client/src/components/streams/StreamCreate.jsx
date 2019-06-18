b7bcba3a76d265145979a0180afd91abd741ced4
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2019-06-18 19:01:17 -0400

    feat(streamy): implemented stream create

--- packages/streams/client/src/components/streams/StreamCreate.jsx
+++ packages/streams/client/src/components/streams/StreamCreate.jsx
@@ -1,60 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import { createStream, setLoading } from '../../actions';
-import InputField from '../../common/InputField';
+import StreamForm from './StreamForm';
 
-const StreamCreate = ({ handleSubmit, createStream, loading, setLoading }) => {
-  const [loadingButton, setLoadingButton] = useState('ui button primary');
-
-  useEffect(() => {
-    loading
-      ? setLoadingButton('ui button disabled')
-      : setLoadingButton('ui button primary');
-  }, [loading]);
-
+const StreamCreate = ({ createStream, loading, setLoading }) => {
   const onSubmit = (formData) => {
     setLoading(true);
     createStream(formData);
   };
 
   return (
-    <form className={'ui form error'} onSubmit={handleSubmit(onSubmit)}>
-      <Field name={'title'} component={InputField} label={'Enter a title'} />
-      <br />
-      <Field
-        name={'description'}
-        component={InputField}
-        label={'Enter a description'}
-      />
-      <button className={loadingButton} type={'submit'}>
-        Create
-      </button>
-    </form>
+    <section>
+      <h3>Stream create</h3>
+      <StreamForm loading={loading} onSubmit={onSubmit} />
+    </section>
   );
 };
-
-const validate = (formValues) => {
-  const errors = {};
-
-  if (
-    !formValues.title ||
-    (formValues.title && formValues.title.trim().length === 0)
-  ) {
-    errors.title = 'You must enter a title.';
-  }
-  if (!formValues.description || formValues.description.trim().length === 0) {
-    errors.description = 'You must enter a description.';
-  }
-
-  return errors;
-};
-
-const formWrap = reduxForm({
-  form: 'streamCreate',
-  validate,
-})(StreamCreate);
 
 const mapStateToProps = (state) => ({
   loading: state.streams.loading,
@@ -66,4 +28,4 @@ export default connect(
     createStream,
     setLoading,
   },
-)(formWrap);
+)(StreamCreate);
