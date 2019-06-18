2a2c59b4e6044f76617ceda3423179249caa7c16
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2019-06-18 19:01:06 -0400

    feat(streamy): implemented stream list

--- packages/streams/client/src/components/streams/StreamList.jsx
+++ packages/streams/client/src/components/streams/StreamList.jsx
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const StreamList = (props) => {
-  return <div>Hola</div>;
+import { fetchStreams } from '../../actions';
+import RenderLoading from '../../common/RenderLoading';
+
+const StreamList = ({
+  fetchStreams,
+  loading,
+  streams,
+  currentUserId,
+  isSignedIn,
+}) => {
+  useEffect(() => {
+    fetchStreams();
+  }, []);
+
+  const renderAdminButtons = (stream) =>
+    stream.userId === currentUserId && (
+      <div className={'right floated content'}>
+        <Link to={`streams/edit/${stream.id}`} className={'ui button primary'}>
+          Edit
+        </Link>
+        <Link
+          to={`streams/delete/${stream.id}`}
+          className={'ui button negative'}
+        >
+          Delete
+        </Link>
+      </div>
+    );
+
+  const renderCreate = () =>
+    isSignedIn && (
+      <div style={{ textAlign: 'right' }}>
+        <Link to={'/streams/new'} className={'ui button primary'}>
+          Create stream
+        </Link>
+      </div>
+    );
+
+  const renderList = () => (
+    <div className={'ui list'}>
+      <RenderLoading loading={loading} />
+      {streams.map((stream) => (
+        <>
+          <div className={'item'} key={stream.id}>
+            <i className={'large middle aligned icon camera'} />
+            <div className={'content'}>
+              {renderAdminButtons(stream)}
+              {stream.title}
+              <div className={'description'}>{stream.description}</div>
+            </div>
+          </div>
+        </>
+      ))}
+      {renderCreate()}
+    </div>
+  );
+
+  return (
+    <section className={'ui segment'}>
+      <h2>Streams</h2>
+      {renderList()}
+    </section>
+  );
 };
 
-export default StreamList;
+const mapStateToProps = (state) => ({
+  streams: Object.values(state.streams.streams),
+  currentUserId: state.auth.userId,
+  loading: state.streams.loading,
+  isSignedIn: state.auth.isSignedIn,
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchStreams },
+)(StreamList);
