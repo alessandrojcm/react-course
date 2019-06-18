f974b6e3e63042ec870d56f8094e829ec5a6af74
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2019-06-18 19:02:20 -0400

    feat(streamy): implemented custom modal

---
+++ packages/streams/client/src/components/Modal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = ({ header, content, children, onDismiss }) => {
+  return ReactDOM.createPortal(
+    <div
+      className={'ui dimmer modals visible active'}
+      onClick={onDismiss}
+    >
+      <section
+        className={'ui standard modal visible active'}
+        onClick={(event) => event.stopPropagation()}
+      >
+        <header className={'header'}>{header}</header>
+        <main className={'content'}>{content}</main>
+        <footer className={'actions'}>
+          {children}
+        </footer>
+      </section>
+    </div>,
+    document.querySelector('#modal'),
+  );
+};
+
+export default Modal;
