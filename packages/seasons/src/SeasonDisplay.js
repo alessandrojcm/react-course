082e3bda78bb0fc2ea958eeca8e8cc4bc9885948
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2019-06-27 13:11:06 -0400

    refactor(seasons): refactor seasons to use hooks

--- packages/seasons/src/SeasonDisplay.js
+++ packages/seasons/src/SeasonDisplay.js
@@ -1,43 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
 import './SeasonDisplay.css';
-import React from 'react';
 
 const seasonConfig = {
   summer: { text: "Let's hit the beach!", icon: 'sun' },
   winter: { text: "Burr it's chilly!", icon: 'snowflake' },
 };
 
-class SeasonDisplay extends React.Component {
-  state = { season: null };
+const SeasonDisplay = ({ lat }) => {
+  const [season, setSeason] = useState('');
 
-  componentDidMount() {
-    this.getSeason(new Date().getMonth());
-  }
-
-  render() {
-    if (!this.state.season) {
-      return <div>Loading...</div>;
-    }
-    const { text, icon } = seasonConfig[this.state.season];
-    return (
-      <div className={`season-display ${this.state.season}`}>
-        <i className={`icon-left massive ${icon} icon`} />
-        <h1>{text}</h1>
-        <i className={`icon-right massive ${icon} icon`} />
-      </div>
-    );
-  }
-
-  getSeason(month) {
+  const getSeason = (month) => {
     if (month > 2 && month < 9) {
-      return this.props.lat > 0
-        ? this.setState({ season: 'summer' })
-        : this.setState({ season: 'winter' });
+      return lat > 0 ? setSeason('summer') : setSeason('winter');
     }
 
-    return this.props.lat < 0
-      ? this.setState({ season: 'summer' })
-      : this.setState({ season: 'winter' });
+    return lat < 0 ? setSeason('summer') : setSeason('winter');
+  };
+
+  useEffect(() => getSeason(new Date().getMonth(), []));
+
+  if (!season) {
+    return <div>Loading...</div>;
   }
-}
+
+  const { text, icon } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${icon} icon`} />
+    </div>
+  );
+};
 
 export default SeasonDisplay;
--- packages/seasons/src/index.js
+++ packages/seasons/src/index.js
@@ -1,5 +1,29 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+
+import LoaderSpinner from './LoaderSpinner';
+import SeasonDisplay from './SeasonDisplay.js';
+
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErroMessage] = useState('');
+
+  const getLatitude = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => setLat(position.coords.latitude),
+      (err) => setErroMessage(err.message),
+    );
+  };
+
+  useEffect(() => getLatitude(), []);
+
+  if (errorMessage && !lat) {
+    return <div>Error: {errorMessage}</div>;
+  } else if (!errorMessage && lat) {
+    return <SeasonDisplay lat={lat} />;
+  }
+
+  return <LoaderSpinner message={'Please accept the location request.'} />;
+};
 
 render(<App />, document.querySelector('#root'));
